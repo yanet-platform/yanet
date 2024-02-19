64a660b8a8edc00c93c12c90de7a9be27a149dd4
Timur Aitov <taitov@yandex-team.ru>
2024-02-19 20:33:51 +0300

    dump neighbor's MAC from kernel on neighbor_update_interfaces

--- dataplane/neighbor.cpp
+++ dataplane/neighbor.cpp
@@ -28,7 +28,7 @@ static void parse_rt_attributes(rtattr* rt_attributes[],
 
 	if (length)
 	{
-		YANET_LOG_WARNING("invalid length: %u of %u\n", length, rta->rta_len);
+		YANET_LOG_WARNING("invalid length: %d of %u\n", length, rta->rta_len);
 	}
 }
 
@@ -54,44 +54,53 @@ static void netlink_parse(const std::function<void(const std::string&, const com
 		nlmsghdr* nl_message_header = (nlmsghdr*)(buffer + offset);
 		uint32_t length = nl_message_header->nlmsg_len;
 
-		ndmsg* nl_message = (ndmsg*)NLMSG_DATA(nl_message_header);
-		parse_rt_attributes(rt_attributes,
-		                    NDA_MAX,
-		                    (rtattr*)(((char*)(nl_message)) + NLMSG_ALIGN(sizeof(ndmsg))),
-		                    nl_message_header->nlmsg_len - NLMSG_LENGTH(sizeof(*nl_message)));
-
-		if (rt_attributes[NDA_DST] &&
-		    rt_attributes[NDA_LLADDR] &&
-		    nl_message->ndm_ifindex)
+		if (nl_message_header->nlmsg_type == NLMSG_DONE ||
+		    nl_message_header->nlmsg_type == NLMSG_ERROR)
 		{
-			std::string interface_name = iface_id_to_name(nl_message->ndm_ifindex);
+			return;
+		}
 
-			unsigned int family = nl_message->ndm_family;
-			if (family == AF_BRIDGE)
+		if (nl_message_header->nlmsg_type == RTM_NEWNEIGH ||
+		    nl_message_header->nlmsg_type == RTM_GETNEIGH)
+		{
+			ndmsg* nl_message = (ndmsg*)NLMSG_DATA(nl_message_header);
+			parse_rt_attributes(rt_attributes,
+			                    NDA_MAX,
+			                    (rtattr*)(((char*)(nl_message)) + NLMSG_ALIGN(sizeof(ndmsg))),
+			                    nl_message_header->nlmsg_len - NLMSG_LENGTH(sizeof(*nl_message)));
+
+			if (rt_attributes[NDA_DST] &&
+			    rt_attributes[NDA_LLADDR] &&
+			    nl_message->ndm_ifindex)
 			{
-				if (RTA_PAYLOAD(rt_attributes[NDA_DST]) == sizeof(in6_addr))
+				std::string interface_name = iface_id_to_name(nl_message->ndm_ifindex);
+
+				unsigned int family = nl_message->ndm_family;
+				if (family == AF_BRIDGE)
 				{
-					family = AF_INET6;
+					if (RTA_PAYLOAD(rt_attributes[NDA_DST]) == sizeof(in6_addr))
+					{
+						family = AF_INET6;
+					}
+					else
+					{
+						family = AF_INET;
+					}
 				}
-				else
+
+				common::mac_address_t mac_address((const uint8_t*)RTA_DATA(rt_attributes[NDA_LLADDR]));
+
+				if (family == AF_INET)
 				{
-					family = AF_INET;
+					common::ipv4_address_t ip_address(rte_be_to_cpu_32(*(const uint32_t*)RTA_DATA(rt_attributes[NDA_DST])));
+					callback(interface_name, ip_address, mac_address);
+				}
+				else if (family == AF_INET6)
+				{
+					common::ipv6_address_t ip_address((const uint8_t*)RTA_DATA(rt_attributes[NDA_DST]));
+					callback(interface_name, ip_address, mac_address);
 				}
 			}
-
-			common::ip_address_t ip_address;
-			if (family == AF_INET)
-			{
-				ip_address = common::ipv4_address_t(rte_be_to_cpu_32(*(const uint32_t*)RTA_DATA(rt_attributes[NDA_DST])));
-			}
-			else
-			{
-				ip_address = common::ipv6_address_t((const uint8_t*)RTA_DATA(rt_attributes[NDA_DST]));
-			}
-
-			common::mac_address_t mac_address((const uint8_t*)RTA_DATA(rt_attributes[NDA_LLADDR]));
-
-			callback(interface_name, ip_address, mac_address);
 		}
 
 		offset += NLMSG_ALIGN(length);
@@ -135,6 +144,50 @@ static void netlink_neighbor_monitor(const std::function<void(const std::string&
 		{
 			break;
 		}
+	}
+
+	close(nl_socket);
+}
+
+static void netlink_neighbor_dump(const std::function<void(const std::string&, const common::ip_address_t&, const common::mac_address_t&)>& callback)
+{
+	int nl_socket = socket(AF_NETLINK, SOCK_RAW, NETLINK_ROUTE);
+	if (nl_socket < 0)
+	{
+		YANET_LOG_ERROR("socket(): %s\n", strerror(errno));
+		return;
+	}
+
+	struct
+	{
+		nlmsghdr nl_msg;
+		ndmsg nd_msg;
+		char buf[256];
+	} request;
+
+	memset(&request, 0, sizeof(request));
+	request.nl_msg.nlmsg_len = NLMSG_LENGTH(sizeof(ndmsg));
+	request.nl_msg.nlmsg_type = RTM_GETNEIGH;
+	request.nl_msg.nlmsg_flags = NLM_F_DUMP | NLM_F_REQUEST;
+
+	static std::atomic<uint32_t> sequence = time(nullptr);
+	request.nl_msg.nlmsg_seq = sequence++;
+
+	request.nd_msg.ndm_family = AF_UNSPEC;
+
+	if (send(nl_socket, &request, sizeof(request), 0) == -1)
+	{
+		YANET_LOG_WARNING("neighbor_dump: send(): %s\n",
+		                  strerror(errno));
+		close(nl_socket);
+		return;
+	}
+
+	char buffer[8192];
+	int buffer_length = recv(nl_socket, buffer, sizeof(buffer), 0);
+	if (buffer_length > 0)
+	{
+		netlink_parse(callback, buffer, buffer_length);
 	}
 
 	close(nl_socket);
@@ -379,6 +432,67 @@ eResult module::neighbor_update_interfaces(const common::idp::neighbor_update_in
 
 	generation_interface.switch_generation();
 	generation_interface.next_unlock();
+
+	netlink_neighbor_dump([this](const std::string& interface_name,
+	                             const common::ip_address_t& ip_address,
+	                             const common::mac_address_t& mac_address) {
+		tInterfaceId interface_id = 0;
+		{
+			auto lock = generation_interface.current_lock_guard();
+
+			const auto& interface_name_to_id = generation_interface.current().interface_name_to_id;
+			auto it = interface_name_to_id.find(interface_name);
+			if (it == interface_name_to_id.end())
+			{
+				return;
+			}
+			interface_id = it->second;
+		}
+
+		YANET_LOG_DEBUG("netlink: %s, %s -> %s\n",
+		                interface_name.data(),
+		                ip_address.toString().data(),
+		                mac_address.toString().data());
+
+		stats.netlink_neighbor_update++;
+
+		dataplane::neighbor::key key;
+		memset(&key, 0, sizeof(key));
+		key.interface_id = interface_id;
+		key.flags = 0;
+
+		if (ip_address.is_ipv4())
+		{
+			key.address.mapped_ipv4_address = ipv4_address_t::convert(ip_address.get_ipv4());
+		}
+		else
+		{
+			key.address = ipv6_address_t::convert(ip_address.get_ipv6());
+			key.flags |= flag_is_ipv6;
+		}
+
+		value value;
+		memcpy(value.ether_address.addr_bytes, mac_address.data(), RTE_ETHER_ADDR_LEN);
+		value.flags = 0;
+		value.last_update_timestamp = dataplane->get_current_time();
+
+		generation_hashtable.update([this, key, value](neighbor::generation_hashtable& hashtable) {
+			for (auto& [socket_id, hashtable_updater] : hashtable.hashtable_updater)
+			{
+				(void)socket_id;
+				if (!hashtable_updater.get_pointer()->insert_or_update(key, value))
+				{
+					stats.hashtable_insert_error++;
+				}
+				else
+				{
+					stats.hashtable_insert_success++;
+				}
+			}
+			return eResult::success;
+		});
+	});
+
 	return eResult::success;
 }
 
