2a88ab21f25e2494e00ebce1fa58187cfdcbeef0
Ivan Morozko <ivan.morozko01@gmail.com>
2024-06-21 15:24:21 +0300

    Extract action_t class from common/acl.h to new common/actions.h file
    It will be better to have all action-related definitions in a separate
    file

---
+++ common/actions.h
@@ -0,0 +1,63 @@
+#pragma once
+
+#include "stream.h"
+#include <utility>
+
+namespace common
+{
+
+namespace acl
+{
+// class action_t is used to store all non-terminating rule data that
+// shouldn't be stored in common::globalBase::tFlow
+// TODO: I don't think that storing all non-terminating rule data in one struct is a good thing.
+// This will pollute the abstraction and the class size will grow when we will introduce new nonterminating
+// kind of rules. I think that defining a new struct/class for each rule kind and then using it in an std::variant
+// is a preffered approach. If so, then this class should not be called "action_t" and rather "dump_t".
+class action_t
+{
+public:
+	action_t() :
+	        dump_id(0),
+	        dump_tag("")
+	{}
+
+	action_t(std::string dump_tag) :
+	        dump_id(0),
+	        dump_tag(std::move(dump_tag))
+	{}
+
+	inline bool operator==(const action_t& o) const
+	{
+		return std::tie(dump_id, dump_tag) ==
+		       std::tie(o.dump_id, o.dump_tag);
+	}
+
+	inline bool operator!=(const action_t& o) const
+	{
+		return !operator==(o);
+	}
+
+	constexpr bool operator<(const action_t& o) const
+	{
+		return std::tie(dump_id, dump_tag) <
+		       std::tie(o.dump_id, o.dump_tag);
+	}
+
+	void pop(stream_in_t& stream)
+	{
+		stream.pop(dump_id);
+		stream.pop(dump_tag);
+	}
+
+	void push(stream_out_t& stream) const
+	{
+		stream.push(dump_id);
+		stream.push(dump_tag);
+	}
+
+	uint64_t dump_id;
+	std::string dump_tag;
+};
+} // namespace acl
+} // namespace common
