328ae0f42a085ce994906eac0d26e4ed393b5e9c
Ivan Morozko <ivan.morozko01@gmail.com>
2024-06-21 15:58:40 +0300

    Remove unused fileds in controlplane/acl_total_table.cpp
    These fields are unused so remove them

--- controlplane/acl_total_table.h
+++ controlplane/acl_total_table.h
@@ -1,6 +1,5 @@
 #pragma once
 
-#include "acl/bitset.h"
 #include "acl_base.h"
 
 #include "common/acl.h"
@@ -21,26 +20,16 @@ public:
 	unsigned int collect(const unsigned int rule_id, const filter& filter);
 	void prepare();
 	void compile();
-	void compile_thread(const unsigned int thread_id, const unsigned int threads_count);
+
 
 public:
 	acl::compiler_t* compiler;
 
 	std::map<common::acl::total_key_t, tAclGroupId> table;
 
-	std::vector<tAclGroupId> remap_group_ids;
-	tAclGroupId group_id;
-
 	std::vector<filter> filters;
 	std::map<filter, unsigned int> filter_ids;
-	std::vector<unsigned int> filter_id_by_rule_id;
 	std::vector<std::set<tAclGroupId>> filter_id_group_ids;
-
-	std::vector<uint8_t> bitmask; /// @todo: bitmask_t
-
-	std::unordered_map<bitset_t, tAclGroupId> map;
-	std::map<tAclGroupId, bitset_t> reverse_map;
-	std::map<tAclGroupId, bitset_t> reverse_map_next;
 };
 
 }
