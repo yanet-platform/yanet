2381df504f103c8e2eecaa4c956b3b886a31f068
Ivan Morozko <ivan.morozko01@gmail.com>
2024-09-25 14:04:29 +0400

    Use [[maybe_unused]] instead of YANET_UNUSED
    This macro will be used to mark structured bindings due to bug in gcc
    (See next commit).
    For unused functions, we can just stick with a regular one

--- controlplane/telegraf.cpp
+++ controlplane/telegraf.cpp
@@ -4,7 +4,7 @@
 using common::int64;
 using controlplane::module::telegraf;
 
-YANET_UNUSED
+[[maybe_unused]]
 static inline double perSecond(const int64_t& valueDiff, const uint64_t& timeDiff)
 {
 	if (timeDiff == 0)
