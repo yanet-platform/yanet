bf6bf72c8a5cb66b6b9d0ed0d7c19c551bcdce0b
Ivan Morozko <ivan.morozko01@gmail.com>
2024-09-25 13:48:28 +0400

    Fix usage of deprecated C++ headers like 'signal.h' or 'stdio.h'
    One should consider using 'csignal'/'cstdio' instead.
    Also we should use cstdint instead of inttypes.h for fixed-size types

--- controlplane/isystem.cpp
+++ controlplane/isystem.cpp
@@ -4,9 +4,9 @@
 #include <net/if_arp.h>
 #include <netdb.h>
 #include <netinet/in.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
 #include <sys/ioctl.h>
 #include <sys/socket.h>
 #include <unistd.h>
