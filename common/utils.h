f07ec617c8a0af9b8964e1616622d62270ee5c49
Ivan Morozko <ivan.morozko01@gmail.com>
2024-09-30 19:24:33 +0400

    Add some type traits to utils.h
    - is_optional_v for std::optional detection.
    - is_variant_v for std::variant detection.
    - is_container_v for general container detection.
    - is_vector_v for std::vector detection.
    - is_set_v for std::set detection.

--- common/utils.h
+++ common/utils.h
@@ -1,7 +1,13 @@
 
 #pragma once
 
+#include <iomanip>
+#include <iterator>
+#include <optional>
+#include <set>
 #include <type_traits>
+#include <variant>
+#include <vector>
 
 namespace utils
 {
@@ -9,6 +15,71 @@ namespace utils
 template<typename>
 struct always_false : std::false_type
 {};
+
+template<typename T>
+struct is_optional : std::false_type
+{};
+
+template<typename T>
+struct is_optional<std::optional<T>> : std::true_type
+{};
+
+template<typename T>
+inline constexpr bool is_optional_v = is_optional<T>::value;
+
+template<typename T>
+struct is_variant : std::false_type
+{};
+
+template<typename... Types>
+struct is_variant<std::variant<Types...>> : std::true_type
+{};
+
+template<typename T>
+inline constexpr bool is_variant_v = is_variant<T>::value;
+
+template<typename T, typename = void>
+struct is_container : std::false_type
+{};
+
+/*
+ * Checks:
+ * Whether std::begin(T&), std::end(T&) and std::empty(T&) are valid expressions.
+ * Whether the result of std::begin(T&) can be dereferenced (i.e., *std::begin(T&) is valid)
+ */
+template<typename T>
+struct is_container<T,
+                    std::void_t<
+                            decltype(std::begin(std::declval<T&>())),
+                            decltype(std::end(std::declval<T&>())),
+                            decltype(std::empty(std::declval<T&>())),
+                            decltype(*std::begin(std::declval<T&>()))>> : std::true_type
+{};
+
+template<typename T>
+inline constexpr bool is_container_v = is_container<T>::value;
+
+template<typename T>
+struct is_vector : std::false_type
+{};
+
+template<typename T, typename Alloc>
+struct is_vector<std::vector<T, Alloc>> : std::true_type
+{};
+
+template<typename T>
+inline constexpr bool is_vector_v = is_vector<T>::value;
+
+template<typename T>
+struct is_set : std::false_type
+{};
+
+template<typename Key, typename Compare, typename Allocator>
+struct is_set<std::set<Key, Compare, Allocator>> : std::true_type
+{};
+
+template<typename T>
+inline constexpr bool is_set_v = is_set<T>::value;
 
 }
 // namespace utils
