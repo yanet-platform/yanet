1018c9514d71f805eb77a7a99ed4e52abcf0b4e6
Ivan Morozko <ivan.morozko01@gmail.com>
2024-12-10 19:13:05 +0400

    Add a new dump_format option to the sharedMemory section in dataplane.conf.
    This option specifies the format for dumping packets in shared memory.
    If the option is not specified or is set to an invalid value,
    the format defaults to raw, maintaining the current behavior.
    
    Valid options are "raw" and "pcap".
    
    The logic for dumping packets in pcap format within shared memory will be
    implemented in subsequent commits.

--- dataplane/sharedmemory.h
+++ dataplane/sharedmemory.h
@@ -1,8 +1,12 @@
+#pragma once
+
 #include <rte_mbuf.h>
 
 #include "common/bufferring.h"
 #include "common/result.h"
 #include "common/type.h"
+
+#include "config.h"
 
 namespace sharedmemory
 {
@@ -11,10 +15,19 @@ using ring_header_t = common::bufferring::ring_header_t;
 using ring_t = common::bufferring::ring_t;
 using item_header_t = common::bufferring::item_header_t;
 using item_t = common::bufferring::item_t;
+using DumpFormat = tDataPlaneConfig::DumpFormat;
 
 class cSharedMemory
 {
+	DumpFormat format_;
+
 public:
+	cSharedMemory() :
+	        format_(DumpFormat::kRaw) {}
+
+	cSharedMemory(DumpFormat format) :
+	        format_(format) {}
+
 	eResult init(void* memory, int unit_size, int units_number);
 	void write(rte_mbuf* mbuf, common::globalBase::eFlowType flow_type);
 
