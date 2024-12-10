b63ce5a8907ed8f101ed3e79687babe241a3097c
Ivan Morozko <ivan.morozko01@gmail.com>
2024-12-10 19:13:05 +0400

    Add a new dump_format option to the sharedMemory section in dataplane.conf.
    This option specifies the format for dumping packets in shared memory.
    If the option is not specified or is set to an invalid value,
    the format defaults to raw, maintaining the current behavior.
    
    Valid options are "raw" and "pcap".
    
    The logic for dumping packets in pcap format within shared memory will be
    implemented in subsequent commits.

--- dataplane/dataplane.cpp
+++ dataplane/dataplane.cpp
@@ -36,6 +36,7 @@
 #include "common/tsc_deltas.h"
 #include "dataplane.h"
 #include "dataplane/sdpserver.h"
+#include "sharedmemory.h"
 #include "globalbase.h"
 #include "sock_dev.h"
 #include "work_runner.h"
@@ -1622,7 +1623,8 @@ eResult cDataPlane::allocateSharedMemory()
 	std::map<tSocketId, uint64_t> shm_size_per_socket;
 	for (const auto& ring_cfg : config.shared_memory)
 	{
-		const auto& [dump_size, dump_count] = ring_cfg.second;
+		const auto& [format, dump_size, dump_count] = ring_cfg.second;
+		GCC_BUG_UNUSED(format);
 
 		auto unit_size = sizeof(sharedmemory::item_header_t) + dump_size;
 		if (unit_size % RTE_CACHE_LINE_SIZE != 0)
@@ -1726,7 +1728,7 @@ eResult cDataPlane::splitSharedMemoryPerWorkers()
 		int ring_id = 0;
 		for (const auto& [tag, ring_cfg] : config.shared_memory)
 		{
-			const auto& [dump_size, units_number] = ring_cfg;
+			const auto& [format, dump_size, units_number] = ring_cfg;
 
 			auto unit_size = sizeof(sharedmemory::item_header_t) + dump_size;
 			if (unit_size % RTE_CACHE_LINE_SIZE != 0)
@@ -1746,7 +1748,7 @@ eResult cDataPlane::splitSharedMemoryPerWorkers()
 
 			auto memaddr = (void*)((intptr_t)shm + offset);
 
-			sharedmemory::cSharedMemory ring;
+			sharedmemory::cSharedMemory ring(format);
 
 			ring.init(memaddr, unit_size, units_number);
 
@@ -2266,6 +2268,7 @@ eResult cDataPlane::parseSharedMemory(const nlohmann::json& json)
 		std::string tag = shmJson["tag"];
 		unsigned int size = shmJson["dump_size"];
 		unsigned int count = shmJson["dump_count"];
+		std::string format_str = shmJson.value("dump_format", "raw");
 
 		if (exist(config.shared_memory, tag))
 		{
@@ -2273,7 +2276,7 @@ eResult cDataPlane::parseSharedMemory(const nlohmann::json& json)
 			return eResult::invalidConfigurationFile;
 		}
 
-		config.shared_memory[tag] = {size, count};
+		config.shared_memory[tag] = {format_str, size, count};
 	}
 
 	return eResult::success;
