dac7a1ac0c2fa44e3cbd6d05f7d3eb41ad0585e1
Ivan Morozko <ivan.morozko01@gmail.com>
2024-12-10 19:13:05 +0400

    Add a new dump_format option to the sharedMemory section in dataplane.conf.
    This option specifies the format for dumping packets in shared memory.
    If the option is not specified or is set to an invalid value,
    the format defaults to raw, maintaining the current behavior.
    
    Valid options are "raw" and "pcap".
    
    The logic for dumping packets in pcap format within shared memory will be
    implemented in subsequent commits.

--- dataplane/config.h
+++ dataplane/config.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "common/define.h"
 #include "common/type.h"
 #include "rte_ethdev.h"
 #include <set>
@@ -9,6 +10,61 @@ using InterfaceName = std::string;
 
 struct tDataPlaneConfig
 {
+	enum class DumpFormat
+	{
+		kRaw,
+		kPcap
+	};
+
+	// TODO: add here path, prefix, pcap files count? like std::variant if format == pcap?
+	struct DumpConfig
+	{
+		DumpFormat format;
+		unsigned int size;
+		unsigned int count;
+
+		DumpConfig() :
+		        format(DumpFormat::kRaw), size(0), count(0)
+		{
+		}
+
+		DumpConfig(std::string_view format_str, unsigned int size, unsigned int count) :
+		        format(StringToDumpFormat(format_str)), size(size), count(count)
+		{
+		}
+
+		// TODO: temporary, adjust after we refactor cli?
+		[[nodiscard]] std::string ToString() const
+		{
+			return DumpFormatToString(format) + " " + std::to_string(size) + " " + std::to_string(count);
+		}
+	};
+
+	static DumpFormat StringToDumpFormat(std::string_view format_str)
+	{
+		if (format_str == "raw")
+			return DumpFormat::kRaw;
+		else if (format_str == "pcap")
+			return DumpFormat::kPcap;
+
+		YANET_LOG_WARNING("Invalid dump format %s, will use raw format", format_str.data());
+		return DumpFormat::kRaw;
+	}
+
+	static std::string DumpFormatToString(DumpFormat format)
+	{
+		switch (format)
+		{
+			case DumpFormat::kRaw:
+				return "raw";
+			case DumpFormat::kPcap:
+				return "pcap";
+			default:
+				YANET_THROW("Invalid dump format");
+				std::abort();
+		}
+	}
+
 	/*
 	   DPDK ports used by `dataplane`.
 	   Each port has a name with which is exposed into host system
@@ -35,7 +91,7 @@ struct tDataPlaneConfig
 	uint32_t SWICMPOutRateLimit = 0;
 	uint32_t rateLimitDivisor = 1;
 	std::string memory;
-	std::map<std::string, std::tuple<unsigned int, unsigned int>> shared_memory;
+	std::map<std::string, DumpConfig> shared_memory;
 
 	std::vector<std::string> ealArgs;
 	std::set<InterfaceName> WorkersInterfaces(std::set<tCoreId> cores)
