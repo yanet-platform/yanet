cd37c2e4f2ddd40198d404c8045fed7d8ecb903e
Aleksandr Stepanov <stal76@yandex.ru>
2024-12-22 15:40:09 +0300

    Corrections before making changes to the VRF
    - Fixed filling in lpm stats.
    - Checking that the route table is ignored has been moved.

--- controlplane/route.h
+++ controlplane/route.h
@@ -177,6 +177,19 @@ public:
 		}
 	}
 
+	bool is_ignored_table(const std::string& table_name) const
+	{
+		for (const auto& [name, module] : routes)
+		{
+			YANET_GCC_BUG_UNUSED(name);
+			if (exist(module.ignore_tables, table_name))
+			{
+				return true;
+			}
+		}
+		return false;
+	}
+
 public:
 	std::map<std::string, controlplane::route::config_t> routes;
 	std::map<ip_prefix_t, std::tuple<tInterfaceId, std::string>> interface_by_neighbors;
