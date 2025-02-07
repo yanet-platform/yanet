a94d9f8954eef66b0324a37bbc831bd1b9bd1a21
Ivan Morozko <ivan.morozko01@gmail.com>
2025-02-07 17:51:43 +0300

    Move FlatMap and FlatSet definitions to acl_base.h
    Those are useful structures which we will use not only
    in acl_transport_table, but in other parts of ACL too

--- controlplane/acl_base.h
+++ controlplane/acl_base.h
@@ -8,6 +8,25 @@
 
 using tAclGroupId = uint32_t;
 
+#if defined(CUSTOM_HASH_STRUCTURES)
+#include "hash_table7.hpp"
+#include "unordered_dense.h"
+
+// TODO: check another maps/hashes. Note that they should work with gcc 7.5 on Ubuntu18
+template<typename Key, typename Value>
+using FlatMap = emhash7::HashMap<Key, Value>;
+template<typename Key>
+using FlatSet = ankerl::unordered_dense::set<Key>;
+#else
+#include <unordered_map>
+#include <unordered_set>
+
+template<typename Key, typename Value>
+using FlatMap = std::unordered_map<Key, Value>;
+template<typename Key>
+using FlatSet = std::unordered_set<Key>;
+#endif
+
 namespace acl
 {
 class compiler_t;
