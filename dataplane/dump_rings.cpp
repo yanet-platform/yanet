bfebf5d1eed3e608118881a9dffa4969bdcd4c8b
Ivan Morozko <ivan.morozko01@gmail.com>
2025-03-19 17:39:52 +0300

    Make dataplane/sharedmemory.cpp actually manage dump rings
    This file was not related to sharedmemory management at all.
    The naming was bad, since this code manages dump rings. Renamed it
    to dump_rings then.
    
    Dump ring is represented by either RingRaw or RingPcap structure.
    They implement common interface defined in RingBase, which is, write a
    packet and get a packet. RingPcap implementation is incomplete by now
    and will be added in later commits.
    
    Also there's a "GetCapacity" function that helps on ring creation a lot

---
+++ dataplane/dump_rings.cpp
@@ -0,0 +1,125 @@
+#include "dump_rings.h"
+#include "RawPacket.h"
+#include "common/type.h"
+#include "common/utils.h"
+#include "metadata.h"
+
+#include <iostream>
+
+namespace dumprings
+{
+
+RingRaw::RingRaw(void* memory, size_t max_pkt_size, size_t pkt_count) :
+        buffer_(memory, max_pkt_size, pkt_count), ring_(buffer_.ring)
+{
+	ring_->header.before = 0;
+	ring_->header.after = 0;
+}
+
+void RingRaw::Write(rte_mbuf* mbuf, common::globalBase::eFlowType flow_type, [[maybe_unused]] uint32_t time)
+{
+	// Each ring has its own header, the header contains absolute position
+	// to which next packet should be written. Position has two state:
+	// -- "before" increments immediately before of copying data to memory;
+	// -- "after" increments after copying data.
+	using ring_header_t = PacketBufferRing::ring_header_t;
+
+	uint64_t wpos = (ring_->header.before) % buffer_.units_number;
+	ring_->header.before++;
+	auto* item = utils::ShiftBuffer<item_t*>(ring_->memory, wpos * buffer_.unit_size);
+
+	dataplane::metadata* metadata = YADECAP_METADATA(mbuf);
+
+	uint64_t memory_size = buffer_.unit_size - sizeof(ring_header_t);
+	uint64_t copy_size = RTE_MIN(memory_size, mbuf->data_len);
+
+	item->header.size = copy_size;
+	item->header.tag = metadata->hash;
+	item->header.in_logicalport_id = metadata->in_logicalport_id;
+	item->header.out_logicalport_id = metadata->out_logicalport_id;
+	item->header.flow_type = static_cast<uint8_t>(flow_type);
+
+	memcpy(item->memory, rte_pktmbuf_mtod(mbuf, void*), copy_size);
+
+	YANET_MEMORY_BARRIER_COMPILE;
+
+	ring_->header.after++;
+}
+
+bool RingRaw::GetPacket(pcpp::RawPacket& raw_packet, unsigned pkt_number) const
+{
+	timespec empty_timespec{}; // RingRaw does not support timestamps
+
+	if (pkt_number >= ring_->header.after)
+	{
+		return false;
+	}
+
+	auto* item = utils::ShiftBuffer<item_t*>(ring_->memory, pkt_number * buffer_.unit_size);
+	int raw_data_len = static_cast<int>(item->header.size);
+
+	raw_packet.initWithRawData(item->memory, raw_data_len, empty_timespec);
+
+	return true;
+}
+
+size_t RingRaw::GetCapacity(size_t max_pkt_size, size_t pkt_count)
+{
+	return PacketBufferRing::GetCapacity(max_pkt_size, pkt_count);
+}
+
+RingPcap::RingPcap(void* memory, size_t max_pkt_size, size_t pkt_count)
+{
+	// TODO: Implement
+}
+
+void RingPcap::Write(rte_mbuf* mbuf, [[maybe_unused]] common::globalBase::eFlowType flow_type, uint32_t time)
+{
+	// TODO: implement
+}
+
+bool RingPcap::GetPacket([[maybe_unused]] pcpp::RawPacket& raw_packet, [[maybe_unused]] unsigned pkt_number) const
+{
+	// TODO: implement
+	return false;
+}
+
+size_t RingPcap::GetCapacity(size_t max_pkt_size, size_t pkt_count)
+{
+	// TODO: implement
+	return 0;
+}
+
+size_t GetCapacity(const Config& config)
+{
+	const auto& [format, max_pkt_size, pkt_count] = config;
+
+	switch (format)
+	{
+		case Format::kRaw:
+			return RingRaw::GetCapacity(max_pkt_size, pkt_count);
+		case Format::kPcap:
+			return RingPcap::GetCapacity(max_pkt_size, pkt_count);
+		default:
+			YANET_THROW("Invalid dump format");
+			std::abort();
+	}
+}
+
+std::unique_ptr<RingBase> CreateSharedMemoryDumpRing(const Config& config, void* memory)
+{
+	const auto& [format, max_pkt_size, pkt_count] = config;
+
+	switch (format)
+	{
+		case Format::kRaw:
+			return std::make_unique<RingRaw>(memory, max_pkt_size, pkt_count);
+		case Format::kPcap:
+			return std::make_unique<RingPcap>(memory, max_pkt_size, pkt_count);
+		default:
+			YANET_THROW("Invalid dump format");
+			std::abort();
+	}
+}
+
+} // namespace sharedmemory
