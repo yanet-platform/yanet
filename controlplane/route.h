43d8cdc3a827fcf3d62913ec97a1357088f7e269
vimes <vimes@yandex-team.ru>
2025-03-19 18:47:24 +0300

    Append requests without lambdas

--- controlplane/route.h
+++ controlplane/route.h
@@ -280,6 +280,9 @@ protected:
 
 	std::optional<uint32_t> tunnel_value_insert(const route::tunnel_value_key_t& value_key);
 	void tunnel_value_remove(const uint32_t& value_id);
+	void AddRequestForEachSocket(common::idp::updateGlobalBase::request& globalbase,
+	                             uint32_t value_id,
+	                             common::globalBase::eNexthopType nexthop);
 	void tunnel_value_compile(common::idp::updateGlobalBase::request& globalbase,
 	                          const route::generation_t& generation,
 	                          const uint32_t& value_id,
