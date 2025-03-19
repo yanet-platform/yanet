0d827d5d05f598648329e8643a2d678c6fc76337
Ivan Morozko <ivan.morozko01@gmail.com>
2025-03-19 17:39:52 +0300

    Make dataplane/sharedmemory.cpp actually manage dump rings
    This file was not related to sharedmemory management at all.
    The naming was bad, since this code manages dump rings. Renamed it
    to dump_rings then.
    
    Dump ring is represented by either RingRaw or RingPcap structure.
    They implement common interface defined in RingBase, which is, write a
    packet and get a packet. RingPcap implementation is incomplete by now
    and will be added in later commits.
    
    Also there's a "GetCapacity" function that helps on ring creation a lot

--- common/bufferring.h
+++ common/bufferring.h
@@ -1,3 +1,5 @@
+#pragma once
+
 #include <cstddef>
 #include <cstdint>
 #include <rte_build_config.h>
@@ -24,6 +26,29 @@ struct PacketBufferRing
 {
 	PacketBufferRing() = default;
 
+	// static function, helps to get capacity in Raw dump ring
+	static size_t GetCapacity(size_t ring_size, size_t item_count, size_t unit_size = 0)
+	{
+		if (unit_size == 0)
+		{
+			unit_size = sizeof(item_header_t) + ring_size;
+
+			if (unit_size % RTE_CACHE_LINE_SIZE != 0)
+			{
+				unit_size += RTE_CACHE_LINE_SIZE - unit_size % RTE_CACHE_LINE_SIZE; /// round up
+			}
+		}
+
+		size_t capacity = sizeof(ring_header_t) + unit_size * item_count;
+
+		if (capacity % RTE_CACHE_LINE_SIZE != 0)
+		{
+			capacity += RTE_CACHE_LINE_SIZE - capacity % RTE_CACHE_LINE_SIZE; /// round up
+		}
+
+		return capacity;
+	}
+
 	PacketBufferRing(void* memory, size_t ring_size, size_t item_count) :
 	        unit_size(sizeof(item_header_t) + ring_size), units_number(item_count)
 	{
@@ -32,11 +57,7 @@ struct PacketBufferRing
 			unit_size += RTE_CACHE_LINE_SIZE - unit_size % RTE_CACHE_LINE_SIZE; /// round up
 		}
 
-		capacity = sizeof(ring_header_t) + unit_size * units_number;
-		if (capacity % RTE_CACHE_LINE_SIZE != 0)
-		{
-			capacity += RTE_CACHE_LINE_SIZE - capacity % RTE_CACHE_LINE_SIZE; /// round up
-		}
+		capacity = GetCapacity(ring_size, item_count, unit_size);
 
 		ring = (ring_t*)memory;
 	}
