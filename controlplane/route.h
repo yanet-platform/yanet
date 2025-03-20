dd56283d913caac87adb66e07b1123fa53657fba
vimes <vimes@yandex-team.ru>
2025-03-20 16:29:04 +0300

    Split filling request_interface from per socket requests

--- controlplane/route.h
+++ controlplane/route.h
@@ -283,6 +283,16 @@ protected:
 	void AddRequestForEachSocket(common::idp::updateGlobalBase::request& globalbase,
 	                             uint32_t value_id,
 	                             common::globalBase::eNexthopType nexthop);
+	bool MakePerSocketRequest(
+	        common::idp::updateGlobalBase::request& globalbase,
+	        uint32_t value_id,
+	        const route::tunnel_value_key_t& value_key);
+	std::vector<route::tunnel_value_interface_t>
+	MakeTunnelValueRequestInterface(
+	        common::idp::updateGlobalBase::request& globalbase,
+	        const route::generation_t& generation,
+	        uint32_t value_id,
+	        const route::tunnel_value_key_t& value_key);
 	void tunnel_value_compile(common::idp::updateGlobalBase::request& globalbase,
 	                          const route::generation_t& generation,
 	                          const uint32_t& value_id,
