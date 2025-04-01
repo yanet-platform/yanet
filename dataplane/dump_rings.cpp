01732922c88d47b99bb5c11e1559cd69a6d69b0a
Ivan Morozko <ivan.morozko01@gmail.com>
2025-04-01 17:12:04 +0300

    Flush DumpRing buffer before reading packets in autotests
    When running YANET with autotests, we have two processes - the dataplane and
    an autotest. The dataplane allocates shared memory and creates DumpRing objects
    that use this memory to store captured packets. The autotest creates
    it's own DumpRing object using the same shared memory and uses them to
    obtain stored packets.
    There are two types of these DumpRings: the original RawRing and a RingPcap.
    
    Since a RingPcap format uses libpcap to write packets to a FILE*, it
    uses buffering. In order to make sure we have all the packets in the
    shared memory, first we need to flush the buffer from the DumpRing
    object in dataplane.

--- dataplane/dump_rings.cpp
+++ dataplane/dump_rings.cpp
@@ -123,6 +123,11 @@ void RingPcap::Write(rte_mbuf* mbuf, [[maybe_unused]] common::globalBase::eFlowT
 	dev_.WritePacket(raw_packet);
 }
 
+void RingPcap::Flush()
+{
+	dev_.Flush();
+}
+
 bool RingPcap::GetPacket(pcpp::RawPacket& raw_packet, unsigned pkt_number) const
 {
 	return dev_.GetPacket(raw_packet, pkt_number);
