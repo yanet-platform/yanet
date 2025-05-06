3a5aa323164779fe3368108bdc55a6b27543231a
OrIX219 <artem.kraev74@gmail.com>
2025-05-06 13:11:36 +0300

    Fix local_pool unit tests

--- dataplane/unittest/local_pool.cpp
+++ dataplane/unittest/local_pool.cpp
@@ -7,29 +7,32 @@ TEST(LocalPoolTest, Allocate)
 {
     dataplane::proxy::LocalPool pool;
 
-    EXPECT_EQ(pool.Allocate(1, 1), std::nullopt);
+    uint32_t client_addr = ipv4_address_t::convert(common::ipv4_address_t("192.168.0.1")).address;
+    tPortId client_port = 12345;
 
+    EXPECT_EQ(pool.Allocate(1, 1, client_addr, client_port), std::nullopt);
+    
     common::ipv4_prefix_t prefix("192.168.0.0/24");
     uint32_t ip1 = ipv4_address_t::convert(common::ipv4_address_t("192.168.0.1")).address;
     pool.Add(1, ipv4_prefix_t{ipv4_address_t{prefix.address()}, prefix.mask()});
-    EXPECT_EQ(pool.Allocate(1, 1), std::make_pair(ip1, rte_cpu_to_be_16((uint16_t)1025)));
-    EXPECT_EQ(pool.Allocate(1, 1), std::make_pair(ip1, rte_cpu_to_be_16((uint16_t)1026)));
-    EXPECT_EQ(pool.Allocate(2, 1), std::make_pair(ip1, rte_cpu_to_be_16((uint16_t)1027)));
+    EXPECT_EQ(pool.Allocate(1, 1, client_addr, client_port), std::make_pair(ip1, rte_cpu_to_be_16((uint16_t)1025)));
+    EXPECT_EQ(pool.Allocate(1, 1, client_addr, client_port), std::make_pair(ip1, rte_cpu_to_be_16((uint16_t)1026)));
+    EXPECT_EQ(pool.Allocate(2, 1, client_addr, client_port), std::make_pair(ip1, rte_cpu_to_be_16((uint16_t)1027)));
 
-    EXPECT_EQ(pool.Allocate(1, 2), std::make_pair(ip1, rte_cpu_to_be_16((uint16_t)1025)));
+    EXPECT_EQ(pool.Allocate(1, 2, client_addr, client_port), std::make_pair(ip1, rte_cpu_to_be_16((uint16_t)1025)));
 
     common::ipv4_prefix_t prefix2("192.168.1.0/30");
     uint32_t ip2 = ipv4_address_t::convert(common::ipv4_address_t("192.168.1.2")).address;
     pool.Add(2, ipv4_prefix_t{ipv4_address_t{prefix2.address()}, prefix2.mask()});
-    for(uint16_t i = 1025; i < UINT16_MAX; i++) {pool.Allocate(2, 3);}
-    EXPECT_EQ(pool.Allocate(2, 3), std::make_pair(ip2, rte_cpu_to_be_16((uint16_t)1025)));
-    EXPECT_EQ(pool.Allocate(2, 3), std::make_pair(ip2, rte_cpu_to_be_16((uint16_t)1026)));
+    for(uint16_t i = 1025; i < UINT16_MAX; i++) {pool.Allocate(2, 3, client_addr, client_port);}
+    EXPECT_EQ(pool.Allocate(2, 3, client_addr, client_port), std::make_pair(ip2, rte_cpu_to_be_16((uint16_t)1025)));
+    EXPECT_EQ(pool.Allocate(2, 3, client_addr, client_port), std::make_pair(ip2, rte_cpu_to_be_16((uint16_t)1026)));
 
-    for(uint16_t i = 1025; i < UINT16_MAX-2; i++) {pool.Allocate(2, 3);}
-    EXPECT_EQ(pool.Allocate(2, 3), std::nullopt);
+    for(uint16_t i = 1025; i < UINT16_MAX-2; i++) {pool.Allocate(2, 3, client_addr, client_port);}
+    EXPECT_EQ(pool.Allocate(2, 3, client_addr, client_port), std::nullopt);
 
     pool.Free(3, rte_be_to_cpu_32(ip2), 1337);
-    EXPECT_EQ(pool.Allocate(2, 3), std::make_pair(ip2, rte_cpu_to_be_16((uint16_t)1337)));
+    EXPECT_EQ(pool.Allocate(2, 3, client_addr, client_port), std::make_pair(ip2, rte_cpu_to_be_16((uint16_t)1337)));
 }
 
 }
