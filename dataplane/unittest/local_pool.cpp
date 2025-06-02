e11ae22afe2fbf3897257164d88b0fc6d0a0afb7
OrIX219 <artem.kraev74@gmail.com>
2025-06-02 12:10:13 +0300

    LocalPool benchmark concurrent access

--- dataplane/unittest/local_pool.cpp
+++ dataplane/unittest/local_pool.cpp
@@ -43,19 +43,19 @@ TEST(LocalPoolTest, Benchmark)
 {
     common::ipv4_prefix_t prefix("192.168.0.0/24");
 
-    unsigned int concurrent = std::thread::hardware_concurrency();
-    if (concurrent == 0)
+    unsigned int concurrency = std::thread::hardware_concurrency();
+    if (concurrency == 0)
     {
-        concurrent = 8;
+        concurrency = 8;
     }
     const int samples = 64;
     const int iterations = 8'000'000;
     std::array<std::chrono::duration<double>, samples> allocations;
     std::array<std::chrono::duration<double>, samples> frees;
-    std::array<std::future<std::pair<std::chrono::duration<double>, std::chrono::duration<double>>>, 100> futures;
-    for (int s = 0; s < samples; s += concurrent)
+    std::array<std::future<std::pair<std::chrono::duration<double>, std::chrono::duration<double>>>, samples> futures;
+    for (int s = 0; s < samples; s += concurrency)
     {
-        for (unsigned int j = s; j < s + concurrent && j < samples; j++)
+        for (unsigned int j = s; j < s + concurrency && j < samples; j++)
         {
             futures[j] = std::async(std::launch::async, [&]() -> std::pair<std::chrono::duration<double>, std::chrono::duration<double>> {
                 dataplane::proxy::LocalPool pool;
@@ -66,8 +66,7 @@ TEST(LocalPoolTest, Benchmark)
                 std::chrono::steady_clock::time_point start = std::chrono::steady_clock::now();
                 for (uint32_t i = 0; i < iterations; i++)
                 {
-                    auto local = pool.Allocate(1, 1);
-                    addresses[i] = local;
+                    addresses[i] = pool.Allocate(1, 1);
                 }
                 auto alloc_elapsed = std::chrono::steady_clock::now() - start;
             
@@ -84,7 +83,7 @@ TEST(LocalPoolTest, Benchmark)
                 return std::make_pair(alloc_elapsed, free_elapsed);
             });
         }
-        for (unsigned int i = s; i < s + concurrent && i < samples; i++)
+        for (unsigned int i = s; i < s + concurrency && i < samples; i++)
         {
             auto [alloc, free] = futures[i].get();
             allocations[i] = alloc;
@@ -95,7 +94,97 @@ TEST(LocalPoolTest, Benchmark)
     std::sort(allocations.begin(), allocations.end());
     std::sort(frees.begin(), frees.end());
 
-    std::cout << "Samples: " << samples << " Concurrent: " << concurrent << "\nIterations: " << iterations << "\n";
+    std::cout << "Samples: " << samples << " (Concurrent: " << concurrency << ")\nIterations: " << iterations << "\n";
+    std::cout << "Allocate:\n\tmin: " << std::chrono::duration_cast<std::chrono::milliseconds>(allocations[0]).count()
+              << "ms, max: " << std::chrono::duration_cast<std::chrono::milliseconds>(allocations[allocations.size() - 1]).count()
+              << "ms, mean: " << std::chrono::duration_cast<std::chrono::milliseconds>(allocations[allocations.size() / 2]).count() << "ms\n";
+    std::cout << "Free:\n\tmin: " << std::chrono::duration_cast<std::chrono::milliseconds>(frees[0]).count()
+              << "ms, max: " << std::chrono::duration_cast<std::chrono::milliseconds>(frees[frees.size() - 1]).count()
+              << "ms, mean: " << std::chrono::duration_cast<std::chrono::milliseconds>(frees[frees.size() / 2]).count() << "ms\n";
+}
+
+TEST(LocalPoolTest, BenchmarkConcurrent)
+{
+    common::ipv4_prefix_t prefix("192.168.0.0/24");
+
+    unsigned int concurrency = std::thread::hardware_concurrency();
+    if (concurrency == 0)
+    {
+        concurrency = 8;
+    }
+    unsigned int sample_concurrency = std::sqrt(concurrency);
+    unsigned int access_concurrency = sample_concurrency;
+    
+    const unsigned int samples = 64;
+    const unsigned int iterations = 8'000'000;
+    unsigned int iter_per_future = iterations / access_concurrency;
+    std::array<std::chrono::duration<double>, samples> allocations;
+    std::array<std::chrono::duration<double>, samples> frees;
+    std::array<std::future<std::pair<std::chrono::duration<double>, std::chrono::duration<double>>>, samples> futures;
+    std::cout << "Samples: " << samples << " (Concurrent: " << sample_concurrency 
+              << ")\nIterations: " << iterations << " (Concurrent: " << access_concurrency << ")\n";
+    for (unsigned int s = 0; s < samples; s += sample_concurrency)
+    {
+        for (unsigned int j = s; j < s + sample_concurrency && j < samples; j++)
+        {
+            futures[j] = std::async(std::launch::async, [&]() -> std::pair<std::chrono::duration<double>, std::chrono::duration<double>> {
+                dataplane::proxy::LocalPool pool;
+                pool.Add(ipv4_prefix_t{ipv4_address_t{prefix.address()}, prefix.mask()});
+            
+                std::vector<std::optional<std::pair<uint32_t, uint16_t>>> addresses(iterations);
+                
+                std::vector<std::future<void>> fs(access_concurrency);
+                std::chrono::steady_clock::time_point start = std::chrono::steady_clock::now();
+                for (unsigned int i = 0; i < access_concurrency; i++)
+                {
+                    fs[i] = std::async(std::launch::async, [=, &addresses, &pool]() {
+                        unsigned int start = i * iter_per_future;
+                        for (unsigned int k = start; k < start + iter_per_future; k++)
+                        {
+                            addresses[k] = pool.Allocate(1, 1);
+                        }
+                    });
+                }
+                for (auto& f : fs)
+                {
+                    f.get();
+                }
+                auto alloc_elapsed = std::chrono::steady_clock::now() - start;
+
+                start = std::chrono::steady_clock::now();
+                for (unsigned int i = 0; i < access_concurrency; i++)
+                {
+                    fs[i] = std::async(std::launch::async, [=, &addresses, &pool]() {
+                        unsigned int start = i * iter_per_future;
+                        for (unsigned int k = start; k < start + iter_per_future; k++)
+                        {
+                            if (addresses[k].has_value())
+                                pool.Free(addresses[k]->first, addresses[k]->second);
+                        }
+                    });
+                }
+                for (auto& f : fs)
+                {
+                    f.get();
+                }
+                auto free_elapsed = std::chrono::steady_clock::now() - start;
+            
+                pool._TestFree();
+    
+                return std::make_pair(alloc_elapsed, free_elapsed);
+            });
+        }
+        for (unsigned int i = s; i < s + sample_concurrency && i < samples; i++)
+        {
+            auto [alloc, free] = futures[i].get();
+            allocations[i] = alloc;
+            frees[i] = free;
+        }
+    }
+
+    std::sort(allocations.begin(), allocations.end());
+    std::sort(frees.begin(), frees.end());
+
     std::cout << "Allocate:\n\tmin: " << std::chrono::duration_cast<std::chrono::milliseconds>(allocations[0]).count()
               << "ms, max: " << std::chrono::duration_cast<std::chrono::milliseconds>(allocations[allocations.size() - 1]).count()
               << "ms, mean: " << std::chrono::duration_cast<std::chrono::milliseconds>(allocations[allocations.size() / 2]).count() << "ms\n";
