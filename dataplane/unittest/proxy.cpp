6f3dad63e2f57703556c3ae5bedb0af0e73c52b5
OrIX219 <artem.kraev74@gmail.com>
2025-06-02 14:29:49 +0300

    ServiceConnections benchmark concurrent access

--- dataplane/unittest/proxy.cpp
+++ dataplane/unittest/proxy.cpp
@@ -8,19 +8,19 @@
 
 TEST(ServiceConnectionsTest, Benchmark)
 {
-    unsigned int concurrent = std::thread::hardware_concurrency();
-    if (concurrent == 0)
+    unsigned int concurrency = std::thread::hardware_concurrency();
+    if (concurrency == 0)
     {
-        concurrent = 8;
+        concurrency = 8;
     }
     const int samples = 64;
     const int iterations = 8'000'000;
     std::array<std::chrono::duration<double>, samples> insert;
     std::array<std::chrono::duration<double>, samples> find;
-    std::array<std::future<std::pair<std::chrono::duration<double>, std::chrono::duration<double>>>, 100> futures;
-    for (int s = 0; s < samples; s += concurrent)
+    std::array<std::future<std::pair<std::chrono::duration<double>, std::chrono::duration<double>>>, samples> futures;
+    for (int s = 0; s < samples; s += concurrency)
     {
-        for (unsigned int j = s; j < s + concurrent && j < samples; j++)
+        for (unsigned int j = s; j < s + concurrency && j < samples; j++)
         {
             futures[j] = std::async(std::launch::async, [&]() -> std::pair<std::chrono::duration<double>, std::chrono::duration<double>> {
                 dataplane::proxy::ServiceConnections conn;
@@ -45,7 +45,7 @@ TEST(ServiceConnectionsTest, Benchmark)
                 return std::make_pair(insert_elapsed, find_elapsed);
             });
         }
-        for (unsigned int i = s; i < s + concurrent && i < samples; i++)
+        for (unsigned int i = s; i < s + concurrency && i < samples; i++)
         {
             auto [ie, fe] = futures[i].get();
             insert[i] = ie;
@@ -54,8 +54,94 @@ TEST(ServiceConnectionsTest, Benchmark)
     }
 
     std::sort(insert.begin(), insert.end());
+    std::sort(find.begin(), find.end());
 
-    std::cout << "Samples: " << samples << " Concurrent: " << concurrent << "\nIterations: " << iterations << "\n";
+    std::cout << "Samples: " << samples << " (Concurrent: " << concurrency << ")\nIterations: " << iterations << "\n";
+    std::cout << "TryInsert:\n\tmin: " << std::chrono::duration_cast<std::chrono::milliseconds>(insert[0]).count()
+              << "ms, max: " << std::chrono::duration_cast<std::chrono::milliseconds>(insert[insert.size() - 1]).count()
+              << "ms, mean: " << std::chrono::duration_cast<std::chrono::milliseconds>(insert[insert.size() / 2]).count() << "ms\n";
+    std::cout << "FindAndLock:\n\tmin: " << std::chrono::duration_cast<std::chrono::milliseconds>(find[0]).count()
+              << "ms, max: " << std::chrono::duration_cast<std::chrono::milliseconds>(find[find.size() - 1]).count()
+              << "ms, mean: " << std::chrono::duration_cast<std::chrono::milliseconds>(find[find.size() / 2]).count() << "ms\n";
+}
+
+TEST(ServiceConnectionsTest, BenchmarkConcurrent)
+{
+    unsigned int concurrency = std::thread::hardware_concurrency();
+    if (concurrency == 0)
+    {
+        concurrency = 8;
+    }
+    unsigned int sample_concurrency = std::sqrt(concurrency);
+    unsigned int access_concurrency = sample_concurrency;
+    
+    const unsigned int samples = 64;
+    const unsigned int iterations = 8'000'000;
+    unsigned int iter_per_future = iterations / access_concurrency;
+    std::array<std::chrono::duration<double>, samples> insert;
+    std::array<std::chrono::duration<double>, samples> find;
+    std::array<std::future<std::pair<std::chrono::duration<double>, std::chrono::duration<double>>>, samples> futures;
+    std::cout << "Samples: " << samples << " (Concurrent: " << sample_concurrency 
+              << ")\nIterations: " << iterations << " (Concurrent: " << access_concurrency << ")\n";
+    for (unsigned int s = 0; s < samples; s += sample_concurrency)
+    {
+        for (unsigned int j = s; j < s + sample_concurrency && j < samples; j++)
+        {
+            futures[j] = std::async(std::launch::async, [&]() -> std::pair<std::chrono::duration<double>, std::chrono::duration<double>> {
+                dataplane::proxy::ServiceConnections conn;
+                conn._TestInit(1, iterations);
+            
+                std::vector<std::future<void>> fs(access_concurrency);
+                std::chrono::steady_clock::time_point start = std::chrono::steady_clock::now();
+                for (unsigned int i = 0; i < access_concurrency; i++)
+                {
+                    fs[i] = std::async(std::launch::async, [=, &conn]() {
+                        unsigned int start = i * iter_per_future;
+                        for (unsigned int k = start; k < start + iter_per_future; k++)
+                        {
+                            conn.TryInsert(k, 2, k, 4, dataplane::proxy::ConnectionState::ESTABLISHED, 6, 7, 8);
+                        }
+                    });
+                }
+                for (auto& f : fs)
+                {
+                    f.get();
+                }
+                auto insert_elapsed = std::chrono::steady_clock::now() - start;
+            
+                start = std::chrono::steady_clock::now();
+                for (unsigned int i = 0; i < access_concurrency; i++)
+                {
+                    fs[i] = std::async(std::launch::async, [=, &conn]() {
+                        unsigned int start = i * iter_per_future;
+                        for (unsigned int k = start; k < start + iter_per_future; k++)
+                        {
+                            conn.FindAndLock(k, 2, 7);
+                        }
+                    });
+                }
+                for (auto& f : fs)
+                {
+                    f.get();
+                }
+                auto find_elapsed = std::chrono::steady_clock::now() - start;
+
+                conn._TestFree();
+    
+                return std::make_pair(insert_elapsed, find_elapsed);
+            });
+        }
+        for (unsigned int i = s; i < s + sample_concurrency && i < samples; i++)
+        {
+            auto [ie, fe] = futures[i].get();
+            insert[i] = ie;
+            find[i] = fe;
+        }
+    }
+
+    std::sort(insert.begin(), insert.end());
+    std::sort(find.begin(), find.end());
+
     std::cout << "TryInsert:\n\tmin: " << std::chrono::duration_cast<std::chrono::milliseconds>(insert[0]).count()
               << "ms, max: " << std::chrono::duration_cast<std::chrono::milliseconds>(insert[insert.size() - 1]).count()
               << "ms, mean: " << std::chrono::duration_cast<std::chrono::milliseconds>(insert[insert.size() / 2]).count() << "ms\n";
