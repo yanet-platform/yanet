f01bae4eb252d55b45d230f4d3380e7b499137a3
Andrey Kiselev <kiselevandrew@yandex.ru>
2026-09-18 15:20:12 +0300

    Simplify controlplane isolation flows and tests

--- dataplane/rteflow.cpp
+++ dataplane/rteflow.cpp
@@ -2,126 +2,44 @@
 
 #include "rteflow.h"
 
-bool CreateFlowForIsolateEthernerProtocolOnQueue(uint16_t port_id, uint16_t isolated_queue, uint16_t protocol_type, const char* protocol_name)
+namespace
 {
-	struct rte_flow_error error;
-	struct rte_flow_attr attr;
-	memset(&attr, 0, sizeof(attr));
+rte_flow* CreateQueueFlow(uint16_t port_id, uint16_t isolated_queue, const rte_flow_item* pattern, const std::string& description)
+{
+	rte_flow_attr attr{};
 	attr.ingress = 1;
-
-	// Prepare patterns
-	struct rte_flow_item pattern[2];
-	memset(&pattern[0], 0, sizeof(pattern[0]));
-	memset(&pattern[1], 0, sizeof(pattern[1]));
-
-	// Pattern 0 - Ethernet - Type
-	struct rte_flow_item_eth spec;
-	memset(&spec, 0, sizeof(spec));
-	spec.type = rte_cpu_to_be_16(protocol_type);
-	struct rte_flow_item_eth mask;
-	memset(&mask, 0, sizeof(mask));
-	mask.type = 0xFFFF;
-	pattern[0].type = RTE_FLOW_ITEM_TYPE_ETH;
-	pattern[0].spec = &spec;
-	pattern[0].mask = &mask;
-	// Pattern 1 - End
-	pattern[1].type = RTE_FLOW_ITEM_TYPE_END;
-
-	// Prepare actions
-	struct rte_flow_action_queue queue_action = {.index = isolated_queue};
-	struct rte_flow_action actions[2];
-	actions[0].type = RTE_FLOW_ACTION_TYPE_QUEUE;
-	actions[0].conf = &queue_action;
-	actions[1].type = RTE_FLOW_ACTION_TYPE_END;
-
-	// Create flow
-	struct rte_flow* flow = rte_flow_create(port_id, &attr, pattern, actions, &error);
+	const rte_flow_action_queue queue_action{isolated_queue};
+	const rte_flow_action actions[] = {
+	        {RTE_FLOW_ACTION_TYPE_QUEUE, &queue_action},
+	        {RTE_FLOW_ACTION_TYPE_END, nullptr}};
+	rte_flow_error error{};
+	auto* flow = rte_flow_create(port_id, &attr, pattern, actions, &error);
 	if (!flow)
 	{
-		YANET_LOG_ERROR("Flow creation for %s port_id=%d, queue_id=%d failed: %s\n", protocol_name, port_id, isolated_queue, error.message);
-		return false;
+		YANET_LOG_ERROR("Flow creation for %s port_id=%d, queue_id=%d failed: %s\n", description.c_str(), port_id, isolated_queue, error.message);
 	}
-	YANET_LOG_INFO("Created flow for %s port_id=%d, queue_id=%d\n", protocol_name, port_id, isolated_queue);
-
-	return true;
-}
-
-bool CreateFlowForIsolateEthernerDstMacOnQueue(uint16_t port_id, uint16_t isolated_queue, uint8_t* dst_mac, uint8_t* dst_mac_mask, const char* protocol_name)
-{
-	struct rte_flow_error error;
-	struct rte_flow_attr attr;
-	memset(&attr, 0, sizeof(attr));
-	attr.ingress = 1;
-
-	// Prepare patterns
-	struct rte_flow_item pattern[2];
-	memset(&pattern[0], 0, sizeof(pattern[0]));
-	memset(&pattern[1], 0, sizeof(pattern[1]));
-
-	struct rte_flow_item_eth spec
-	{};
-	struct rte_flow_item_eth mask
-	{};
-	memcpy(spec.hdr.dst_addr.addr_bytes, dst_mac, RTE_ETHER_ADDR_LEN);
-	memcpy(mask.hdr.dst_addr.addr_bytes, dst_mac_mask, RTE_ETHER_ADDR_LEN);
-	pattern[0].type = RTE_FLOW_ITEM_TYPE_ETH;
-	pattern[0].spec = &spec;
-	pattern[0].mask = &mask;
-
-	// Pattern 1 - End
-	pattern[1].type = RTE_FLOW_ITEM_TYPE_END;
-
-	// Prepare actions
-	struct rte_flow_action_queue queue_action = {.index = isolated_queue};
-	struct rte_flow_action actions[2];
-	actions[0].type = RTE_FLOW_ACTION_TYPE_QUEUE;
-	actions[0].conf = &queue_action;
-	actions[1].type = RTE_FLOW_ACTION_TYPE_END;
-
-	// Create flow
-	struct rte_flow* flow = rte_flow_create(port_id, &attr, pattern, actions, &error);
-	if (!flow)
+	else
 	{
-		YANET_LOG_ERROR("Flow creation for %s port_id=%d, queue_id=%d failed: %s\n", protocol_name, port_id, isolated_queue, error.message);
-		return false;
+		YANET_LOG_INFO("Created flow for %s port_id=%d, queue_id=%d\n", description.c_str(), port_id, isolated_queue);
 	}
-	YANET_LOG_INFO("Created flow for %s port_id=%d, queue_id=%d, dst=%s/%s\n", protocol_name, port_id, isolated_queue, common::mac_address_t(spec.hdr.dst_addr.addr_bytes).toString().c_str(), common::mac_address_t(mask.hdr.dst_addr.addr_bytes).toString().c_str());
-
-	return true;
+	return flow;
 }
 
-struct rte_flow* CreateFlowForIsolateIpPrefixOnQueue(uint16_t port_id, uint16_t isolated_queue, const common::ip_prefix_t& prefix)
+rte_flow* CreateFlowForIsolateIpPrefixOnQueue(uint16_t port_id, uint16_t isolated_queue, const common::ip_prefix_t& prefix)
 {
-	struct rte_flow_error error;
-	struct rte_flow_attr attr;
-	memset(&attr, 0, sizeof(attr));
-	attr.ingress = 1;
-
-	// Prepare patterns
-	struct rte_flow_item pattern[3];
-	memset(&pattern[0], 0, sizeof(pattern[0]));
-	memset(&pattern[1], 0, sizeof(pattern[1]));
-	memset(&pattern[2], 0, sizeof(pattern[2]));
-
-	// Pattern 0 - Ethernet
+	rte_flow_item pattern[3]{};
 	pattern[0].type = RTE_FLOW_ITEM_TYPE_ETH;
-	// Pattern 1 - IPv4 or IPv6 - filter on dst prefix
-	struct rte_flow_item_ipv4 ipv4_spec
-	{};
-	struct rte_flow_item_ipv4 ipv4_mask
-	{};
-	struct rte_flow_item_ipv6 ipv6_spec
-	{};
-	struct rte_flow_item_ipv6 ipv6_mask
-	{};
+	pattern[2].type = RTE_FLOW_ITEM_TYPE_END;
+	rte_flow_item_ipv4 ipv4_spec{};
+	rte_flow_item_ipv4 ipv4_mask{};
+	rte_flow_item_ipv6 ipv6_spec{};
+	rte_flow_item_ipv6 ipv6_mask{};
 	if (prefix.is_ipv4())
 	{
 		ipv4_spec.hdr.dst_addr = rte_cpu_to_be_32(prefix.get_ipv4().address());
 		uint8_t mask_len = prefix.get_ipv4().mask();
 		ipv4_mask.hdr.dst_addr = rte_cpu_to_be_32(mask_len == 0 || mask_len > 32 ? 0 : 0xFFFFFFFFu << (32u - mask_len));
-		pattern[1].type = RTE_FLOW_ITEM_TYPE_IPV4;
-		pattern[1].spec = &ipv4_spec;
-		pattern[1].mask = &ipv4_mask;
+		pattern[1] = {RTE_FLOW_ITEM_TYPE_IPV4, &ipv4_spec, nullptr, &ipv4_mask};
 	}
 	else
 	{
@@ -131,58 +49,20 @@ struct rte_flow* CreateFlowForIsolateIpPrefixOnQueue(uint16_t port_id, uint16_t 
 		{
 			ipv6_mask.hdr.dst_addr[i / 8] |= (1u << (7 - i % 8));
 		}
-		pattern[1].type = RTE_FLOW_ITEM_TYPE_IPV6;
-		pattern[1].spec = &ipv6_spec;
-		pattern[1].mask = &ipv6_mask;
+		pattern[1] = {RTE_FLOW_ITEM_TYPE_IPV6, &ipv6_spec, nullptr, &ipv6_mask};
 	}
-
-	// Pattern 2 - End
-	pattern[2].type = RTE_FLOW_ITEM_TYPE_END;
-
-	// Prepare actions
-	struct rte_flow_action_queue queue_action = {.index = isolated_queue};
-	struct rte_flow_action actions[2];
-	actions[0].type = RTE_FLOW_ACTION_TYPE_QUEUE;
-	actions[0].conf = &queue_action;
-	actions[1].type = RTE_FLOW_ACTION_TYPE_END;
-
-	// Create flow
-	struct rte_flow* flow = rte_flow_create(port_id, &attr, pattern, actions, &error);
-	if (!flow)
-	{
-		YANET_LOG_ERROR("Flow creation port_id=%d, queue_id=%d, prefix=%s failed: %s\n", port_id, isolated_queue, prefix.toString().c_str(), error.message);
-		return nullptr;
-	}
-
-	YANET_LOG_INFO("Created flow port_id=%d, queue_id=%d, prefix=%s\n", port_id, isolated_queue, prefix.toString().c_str());
-	return flow;
+	return CreateQueueFlow(port_id, isolated_queue, pattern, prefix.toString());
 }
 
 bool SetupRSSForPortWithIsolatedQueue(uint16_t port_id, uint16_t queues_count, uint16_t isolated_queue)
 {
-	struct rte_flow_error error;
-	struct rte_flow_attr attr;
-	memset(&attr, 0, sizeof(attr));
+	rte_flow_attr attr{};
 	attr.ingress = 1;
 	attr.priority = 1;
-
-	// Prepare patterns
-	struct rte_flow_item pattern[3];
-	memset(&pattern[0], 0, sizeof(pattern[0]));
-	memset(&pattern[1], 0, sizeof(pattern[1]));
-	memset(&pattern[2], 0, sizeof(pattern[2]));
-
-	// Pattern 0 - Ethernet
+	rte_flow_item pattern[3]{};
 	pattern[0].type = RTE_FLOW_ITEM_TYPE_ETH;
-	pattern[0].spec = nullptr;
-	// Pattern 1 - IPv4 or IPv6 without restrictions on addresses
-	pattern[1].type = RTE_FLOW_ITEM_TYPE_IPV4;
-	pattern[1].spec = nullptr;
-	pattern[1].mask = nullptr;
-	// Pattern 1 - End
 	pattern[2].type = RTE_FLOW_ITEM_TYPE_END;
 
-	// Prepare actions
 	std::vector<uint16_t> queues;
 	queues.reserve(queues_count);
 	for (uint32_t i = 0; i < queues_count; i++)
@@ -193,46 +73,49 @@ bool SetupRSSForPortWithIsolatedQueue(uint16_t port_id, uint16_t queues_count, u
 		}
 	}
 
-	struct rte_flow_action_rss action_rss = {
-	        .func = RTE_ETH_HASH_FUNCTION_DEFAULT,
-	        .level = 0,
-	        .types = RTE_ETH_RSS_IP | RTE_ETH_RSS_TCP | RTE_ETH_RSS_UDP,
-	        .key_len = 0,
-	        .queue_num = (uint32_t)queues.size(),
-	        .key = nullptr,
-	        .queue = queues.data(),
-	};
+	rte_flow_action_rss action_rss{};
+	action_rss.func = RTE_ETH_HASH_FUNCTION_DEFAULT;
+	action_rss.types = RTE_ETH_RSS_IP | RTE_ETH_RSS_TCP | RTE_ETH_RSS_UDP;
+	action_rss.queue_num = queues.size();
+	action_rss.queue = queues.data();
+	const rte_flow_action actions[] = {
+	        {RTE_FLOW_ACTION_TYPE_RSS, &action_rss},
+	        {RTE_FLOW_ACTION_TYPE_END, nullptr}};
 
-	struct rte_flow_action actions[2];
-	actions[0].type = RTE_FLOW_ACTION_TYPE_RSS;
-	actions[0].conf = &action_rss;
-	actions[1].type = RTE_FLOW_ACTION_TYPE_END;
-
-	// Create flows for IPv4 and IPv6
-	for (int kind = 0; kind < 2; kind++)
+	for (const auto type : {RTE_FLOW_ITEM_TYPE_IPV4, RTE_FLOW_ITEM_TYPE_IPV6})
 	{
-		pattern[1].type = (kind == 0 ? RTE_FLOW_ITEM_TYPE_IPV4 : RTE_FLOW_ITEM_TYPE_IPV6);
-		struct rte_flow* flow_default = rte_flow_create(port_id, &attr, pattern, actions, &error);
-		if (!flow_default)
+		pattern[1].type = type;
+		rte_flow_error error{};
+		if (!rte_flow_create(port_id, &attr, pattern, actions, &error))
 		{
-			YANET_LOG_ERROR("RSS flow IPv%d creation for port_id=%d and isolated queue=%d failed: %s\n", (kind == 0 ? 4 : 6), port_id, isolated_queue, error.message);
+			YANET_LOG_ERROR("RSS flow IPv%d creation for port_id=%d and isolated queue=%d failed: %s\n", type == RTE_FLOW_ITEM_TYPE_IPV4 ? 4 : 6, port_id, isolated_queue, error.message);
 			return false;
 		}
-
-		YANET_LOG_INFO("Created RSS flow IPv%d for port_id=%d and isolated queue=%d\n", (kind == 0 ? 4 : 6), port_id, isolated_queue);
+		YANET_LOG_INFO("Created RSS flow IPv%d for port_id=%d and isolated queue=%d\n", type == RTE_FLOW_ITEM_TYPE_IPV4 ? 4 : 6, port_id, isolated_queue);
 	}
-
 	return true;
+}
 }
 
 void CreateFlowsForIsolatedPort(uint16_t port_id, uint16_t queues_count, uint16_t isolated_queue)
 {
-	uint8_t stp_mac[] = {0x01, 0x80, 0xc2, 0x00, 0x00, 0x00};
-	uint8_t stp_mac_mask[] = {0xff, 0xff, 0xff, 0xff, 0xff, 0xff};
-	CreateFlowForIsolateEthernerDstMacOnQueue(port_id, isolated_queue, stp_mac, stp_mac_mask, "STP");
+	rte_flow_item_eth spec{};
+	rte_flow_item_eth mask{};
+	const rte_flow_item pattern[] = {
+	        {RTE_FLOW_ITEM_TYPE_ETH, &spec, nullptr, &mask},
+	        {RTE_FLOW_ITEM_TYPE_END, nullptr, nullptr, nullptr}};
+	spec.hdr.dst_addr = {{0x01, 0x80, 0xc2, 0x00, 0x00, 0x00}};
+	memset(mask.hdr.dst_addr.addr_bytes, 0xff, RTE_ETHER_ADDR_LEN);
+	CreateQueueFlow(port_id, isolated_queue, pattern, "STP");
 
-	CreateFlowForIsolateEthernerProtocolOnQueue(port_id, isolated_queue, RTE_ETHER_TYPE_ARP, "ARP");
-	CreateFlowForIsolateEthernerProtocolOnQueue(port_id, isolated_queue, RTE_ETHER_TYPE_LLDP, "LLDP");
+	for (const auto& [protocol, name] : {std::pair{RTE_ETHER_TYPE_ARP, "ARP"}, std::pair{RTE_ETHER_TYPE_LLDP, "LLDP"}})
+	{
+		spec = {};
+		mask = {};
+		spec.hdr.ether_type = rte_cpu_to_be_16(protocol);
+		mask.hdr.ether_type = 0xffff;
+		CreateQueueFlow(port_id, isolated_queue, pattern, name);
+	}
 	SetupRSSForPortWithIsolatedQueue(port_id, queues_count, isolated_queue);
 }
 
@@ -250,7 +133,7 @@ void RteFlowStorage::UpdatePrefixes(const std::set<common::ip_prefix_t>& prefixe
 			auto key = std::make_tuple(port_id, queue_id, prefix);
 			if (flows_.count(key) == 0)
 			{
-				struct rte_flow* flow = CreateFlowForIsolateIpPrefixOnQueue(port_id, queue_id, prefix);
+				auto* flow = CreateFlowForIsolateIpPrefixOnQueue(port_id, queue_id, prefix);
 				if (flow != nullptr)
 				{
 					flows_.emplace(key, flow);
@@ -268,8 +151,7 @@ void RteFlowStorage::UpdatePrefixes(const std::set<common::ip_prefix_t>& prefixe
 			continue;
 		}
 
-		struct rte_flow_error error
-		{};
+		rte_flow_error error{};
 		if (rte_flow_destroy(port_id, iter->second, &error))
 		{
 			YANET_LOG_ERROR("error delete isolated cp flow for prefix: %s, port: %d, queue: %d, message: %s\n", prefix.toString().c_str(), port_id, queue_id, error.message);
--- dataplane/unittest/isolation.cpp
+++ dataplane/unittest/isolation.cpp
@@ -1,20 +1,15 @@
 #include <gtest/gtest.h>
 #include <rte_flow.h>
 
+#include <algorithm>
+#include <cstdlib>
+#include <cstring>
+#include <vector>
+
 #include "dataplane/dataplane.h"
 
 namespace
 {
-
-struct FlowAction
-{
-	tPortId port;
-	rte_flow_action_type type;
-	std::vector<uint16_t> queues;
-};
-
-std::vector<FlowAction> flow_actions;
-
 class IsolationDataPlane : public cDataPlane
 {
 public:
@@ -24,66 +19,64 @@ public:
 		config.use_kernel_interface = false;
 	}
 
+	using cDataPlane::config;
 	using cDataPlane::parseJsonPorts;
+	using cDataPlane::ports;
+};
 
-	void Isolate(std::set<tCoreId> cores)
+struct FlowRule
+{
+	uint16_t port;
+	uint32_t priority;
+	rte_flow_item_type type;
+	rte_flow_action_type action;
+	rte_flow_item_eth eth_spec{};
+	rte_flow_item_eth eth_mask{};
+	rte_flow_item_ipv4 ipv4_spec{};
+	rte_flow_item_ipv4 ipv4_mask{};
+	rte_flow_item_ipv6 ipv6_spec{};
+	rte_flow_item_ipv6 ipv6_mask{};
+	uint16_t queue = 0;
+	std::vector<uint16_t> rss_queues;
+	rte_flow* handle = nullptr;
+};
+
+std::vector<FlowRule> rules;
+std::vector<std::pair<uint16_t, rte_flow*>> destroyed;
+std::vector<void*> allocations;
+size_t allocation_calls = 0;
+int failing_port = -1;
+bool fail_destroy = false;
+uintptr_t next_handle = 1;
+
+class RteFlowTest : public ::testing::Test
+{
+	void SetUp() override
 	{
-		config.workers_isolated_cp = std::move(cores);
+		rules.clear();
+		destroyed.clear();
+		allocation_calls = 0;
+		failing_port = -1;
+		fail_destroy = false;
+		next_handle = 1;
 	}
 
-	const auto& Workers() const
+	void TearDown() override
 	{
-		return config.workers;
-	}
-
-	void AddPort(tPortId port, const std::map<tCoreId, tQueueId>& queues)
-	{
-		ports[port] = {"test", queues, static_cast<unsigned int>(queues.size()), {}, "test", false};
+		for (void* allocation : allocations)
+		{
+			std::free(allocation);
+		}
+		allocations.clear();
 	}
 };
 
-TEST(ControlPlaneIsolation, AssignsDistinctIsolatedCoresToPortsOnSameSocket)
+template<typename T>
+void ExpectItem(const T& actual, const T& expected)
 {
-	IsolationDataPlane dataplane;
-	dataplane.Isolate({8, 10});
-	const nlohmann::json ports = {
-	        {{"interfaceName", "first"}, {"pci", "first"}, {"coreIds", {4}}, {"rssFlags", {"IPV4"}}},
-	        {{"interfaceName", "second"}, {"pci", "second"}, {"coreIds", {6}}, {"rssFlags", {"IPV4"}}}};
-
-	ASSERT_EQ(dataplane.parseJsonPorts(ports), eResult::success);
-	EXPECT_EQ(dataplane.Workers().at(8), std::vector<std::string>{"first"});
-	EXPECT_EQ(dataplane.Workers().at(10), std::vector<std::string>{"second"});
+	EXPECT_EQ(0, std::memcmp(&actual, &expected, sizeof(T)));
 }
-
-TEST(ControlPlaneIsolation, UsesActualQueueMapForIsolatedAndOrdinaryTraffic)
-{
-	IsolationDataPlane dataplane;
-	dataplane.Isolate({2});
-	const nlohmann::json ports = {
-	        {{"interfaceName", "test"}, {"pci", "test"}, {"coreIds", {4, 6}}, {"rssFlags", {"IPV4"}}}};
-	ASSERT_EQ(dataplane.parseJsonPorts(ports), eResult::success);
-	dataplane.AddPort(7, {{2, 0}, {4, 1}, {6, 2}});
-	flow_actions.clear();
-
-	dataplane.StartIsolatedControlPlane();
-
-	ASSERT_EQ(flow_actions.size(), 5u);
-	for (const auto& action : flow_actions)
-	{
-		EXPECT_EQ(action.port, 7);
-		if (action.type == RTE_FLOW_ACTION_TYPE_QUEUE)
-		{
-			EXPECT_EQ(action.queues, std::vector<uint16_t>{0});
-		}
-		else
-		{
-			EXPECT_EQ(action.type, RTE_FLOW_ACTION_TYPE_RSS);
-			EXPECT_EQ(action.queues, (std::vector<uint16_t>{1, 2}));
-		}
-	}
 }
-
-} // namespace
 
 extern "C" int __wrap_numa_node_of_cpu(int)
 {
@@ -91,22 +84,262 @@ extern "C" int __wrap_numa_node_of_cpu(int)
 }
 
 extern "C" rte_flow* __wrap_rte_flow_create(uint16_t port,
-                                            const rte_flow_attr*,
-                                            const rte_flow_item[],
-                                            const rte_flow_action actions[],
-                                            rte_flow_error*)
+                                            const rte_flow_attr* attr,
+                                            const rte_flow_item* pattern,
+                                            const rte_flow_action* actions,
+                                            rte_flow_error* error)
 {
-	FlowAction action{port, actions[0].type, {}};
-	if (action.type == RTE_FLOW_ACTION_TYPE_QUEUE)
+	EXPECT_EQ(1u, attr->ingress);
+	EXPECT_EQ(RTE_FLOW_ITEM_TYPE_ETH, pattern[0].type);
+	FlowRule rule{};
+	rule.port = port;
+	rule.priority = attr->priority;
+	rule.type = pattern[1].type;
+	rule.action = actions[0].type;
+	if (pattern[0].spec)
 	{
-		action.queues.push_back(static_cast<const rte_flow_action_queue*>(actions[0].conf)->index);
+		rule.eth_spec = *static_cast<const rte_flow_item_eth*>(pattern[0].spec);
+		rule.eth_mask = *static_cast<const rte_flow_item_eth*>(pattern[0].mask);
 	}
-	else if (action.type == RTE_FLOW_ACTION_TYPE_RSS)
+	if (pattern[1].spec)
 	{
+		if (pattern[1].type == RTE_FLOW_ITEM_TYPE_IPV4)
+		{
+			rule.ipv4_spec = *static_cast<const rte_flow_item_ipv4*>(pattern[1].spec);
+			rule.ipv4_mask = *static_cast<const rte_flow_item_ipv4*>(pattern[1].mask);
+		}
+		else if (pattern[1].type == RTE_FLOW_ITEM_TYPE_IPV6)
+		{
+			rule.ipv6_spec = *static_cast<const rte_flow_item_ipv6*>(pattern[1].spec);
+			rule.ipv6_mask = *static_cast<const rte_flow_item_ipv6*>(pattern[1].mask);
+		}
+	}
+	EXPECT_EQ(RTE_FLOW_ITEM_TYPE_END, pattern[rule.type == RTE_FLOW_ITEM_TYPE_END ? 1 : 2].type);
+	EXPECT_EQ(RTE_FLOW_ACTION_TYPE_END, actions[1].type);
+	if (actions[0].type == RTE_FLOW_ACTION_TYPE_QUEUE)
+	{
+		rule.queue = static_cast<const rte_flow_action_queue*>(actions[0].conf)->index;
+	}
+	else
+	{
+		EXPECT_EQ(RTE_FLOW_ACTION_TYPE_RSS, actions[0].type);
 		const auto& rss = *static_cast<const rte_flow_action_rss*>(actions[0].conf);
-		action.queues.assign(rss.queue, rss.queue + rss.queue_num);
+		EXPECT_EQ(RTE_ETH_HASH_FUNCTION_DEFAULT, rss.func);
+		EXPECT_EQ(0u, rss.level);
+		EXPECT_EQ(RTE_ETH_RSS_IP | RTE_ETH_RSS_TCP | RTE_ETH_RSS_UDP, rss.types);
+		EXPECT_EQ(nullptr, pattern[1].spec);
+		EXPECT_EQ(nullptr, pattern[1].mask);
+		rule.rss_queues.assign(rss.queue, rss.queue + rss.queue_num);
 	}
-	flow_actions.push_back(std::move(action));
-	static int flow;
-	return reinterpret_cast<rte_flow*>(&flow);
+	if (port == failing_port)
+	{
+		error->message = "injected creation failure";
+	}
+	else
+	{
+		rule.handle = reinterpret_cast<rte_flow*>(next_handle++);
+	}
+	rules.push_back(rule);
+	return rule.handle;
+}
+
+extern "C" int __wrap_rte_flow_destroy(uint16_t port, rte_flow* flow, rte_flow_error* error)
+{
+	destroyed.emplace_back(port, flow);
+	if (fail_destroy)
+	{
+		error->message = "injected destruction failure";
+		return -1;
+	}
+	return 0;
+}
+
+extern "C" void* __wrap_rte_malloc(const char*, size_t size, unsigned)
+{
+	++allocation_calls;
+	void* allocation = std::malloc(std::max(size, sizeof(rte_flow_item_ipv6)));
+	std::memset(allocation, 0xa5, std::max(size, sizeof(rte_flow_item_ipv6)));
+	allocations.push_back(allocation);
+	return allocation;
+}
+
+extern "C" void __wrap_rte_free(void* allocation)
+{
+	allocations.erase(std::remove(allocations.begin(), allocations.end(), allocation), allocations.end());
+	std::free(allocation);
+}
+
+TEST_F(RteFlowTest, IPv6PrefixUsesCompleteZeroedItems)
+{
+	RteFlowStorage storage;
+	storage.AddPortAndQueue(2, 7);
+	storage.UpdatePrefixes({common::ip_prefix_t("2001:db8:1234:5680::/57")});
+	ASSERT_EQ(1u, rules.size());
+	rte_flow_item_ipv6 spec{};
+	rte_flow_item_ipv6 mask{};
+	const uint8_t address[] = {0x20, 0x01, 0x0d, 0xb8, 0x12, 0x34, 0x56, 0x80};
+	const uint8_t prefix_mask[] = {0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0x80};
+	std::memcpy(spec.hdr.dst_addr, address, sizeof(address));
+	std::memcpy(mask.hdr.dst_addr, prefix_mask, sizeof(prefix_mask));
+	ExpectItem(rules[0].ipv6_spec, spec);
+	ExpectItem(rules[0].ipv6_mask, mask);
+	EXPECT_EQ(7u, rules[0].queue);
+}
+
+TEST_F(RteFlowTest, IPv4PrefixMatchesOnlyDestination)
+{
+	RteFlowStorage storage;
+	storage.AddPortAndQueue(2, 7);
+	storage.UpdatePrefixes({common::ip_prefix_t("192.0.2.128/25")});
+	ASSERT_EQ(1u, rules.size());
+	rte_flow_item_ipv4 spec{};
+	rte_flow_item_ipv4 mask{};
+	spec.hdr.dst_addr = rte_cpu_to_be_32(0xc0000280);
+	mask.hdr.dst_addr = rte_cpu_to_be_32(0xffffff80);
+	ExpectItem(rules[0].ipv4_spec, spec);
+	ExpectItem(rules[0].ipv4_mask, mask);
+	EXPECT_EQ(7u, rules[0].queue);
+}
+
+TEST_F(RteFlowTest, StaticRulesPreserveTrafficSelectionAndRssQueues)
+{
+	CreateFlowsForIsolatedPort(2, 4, 1);
+	ASSERT_EQ(5u, rules.size());
+	rte_flow_item_eth spec{};
+	rte_flow_item_eth mask{};
+	const uint8_t stp[] = {0x01, 0x80, 0xc2, 0, 0, 0};
+	std::memcpy(spec.hdr.dst_addr.addr_bytes, stp, sizeof(stp));
+	std::memset(mask.hdr.dst_addr.addr_bytes, 0xff, sizeof(stp));
+	ExpectItem(rules[0].eth_spec, spec);
+	ExpectItem(rules[0].eth_mask, mask);
+	for (size_t i = 1; i < 3; ++i)
+	{
+		spec = {};
+		mask = {};
+		spec.hdr.ether_type = rte_cpu_to_be_16(i == 1 ? RTE_ETHER_TYPE_ARP : RTE_ETHER_TYPE_LLDP);
+		mask.hdr.ether_type = 0xffff;
+		ExpectItem(rules[i].eth_spec, spec);
+		ExpectItem(rules[i].eth_mask, mask);
+	}
+	for (size_t i = 0; i < 3; ++i)
+	{
+		EXPECT_EQ(1u, rules[i].queue);
+		EXPECT_EQ(0u, rules[i].priority);
+	}
+	EXPECT_EQ(RTE_FLOW_ITEM_TYPE_IPV4, rules[3].type);
+	EXPECT_EQ(RTE_FLOW_ITEM_TYPE_IPV6, rules[4].type);
+	for (size_t i = 3; i < 5; ++i)
+	{
+		EXPECT_EQ((std::vector<uint16_t>{0, 2, 3}), rules[i].rss_queues);
+		EXPECT_EQ(1u, rules[i].priority);
+	}
+}
+
+TEST_F(RteFlowTest, TemporaryPatternsDoNotAllocateDpdkMemory)
+{
+	CreateFlowsForIsolatedPort(2, 4, 1);
+	RteFlowStorage storage;
+	storage.AddPortAndQueue(2, 1);
+	storage.UpdatePrefixes({common::ip_prefix_t("192.0.2.0/24"), common::ip_prefix_t("2001:db8::/32")});
+	EXPECT_EQ(0u, allocation_calls);
+}
+
+TEST_F(RteFlowTest, RepeatedUpdateRetriesOnlyPortWhoseCreationFailed)
+{
+	RteFlowStorage storage;
+	storage.AddPortAndQueue(2, 1);
+	storage.AddPortAndQueue(3, 4);
+	const std::set<common::ip_prefix_t> prefixes{common::ip_prefix_t("192.0.2.0/24")};
+	failing_port = 3;
+	storage.UpdatePrefixes(prefixes);
+	ASSERT_EQ(2u, rules.size());
+	failing_port = -1;
+	storage.UpdatePrefixes(prefixes);
+	ASSERT_EQ(3u, rules.size());
+	EXPECT_EQ(3u, rules.back().port);
+	EXPECT_EQ(4u, rules.back().queue);
+	storage.UpdatePrefixes(prefixes);
+	EXPECT_EQ(3u, rules.size());
+}
+
+TEST_F(RteFlowTest, FailedDestroyRetainsHandleForNextUpdate)
+{
+	RteFlowStorage storage;
+	storage.AddPortAndQueue(2, 1);
+	storage.UpdatePrefixes({common::ip_prefix_t("192.0.2.0/24")});
+	ASSERT_EQ(1u, rules.size());
+	const auto handle = rules[0].handle;
+	fail_destroy = true;
+	storage.UpdatePrefixes({});
+	fail_destroy = false;
+	storage.UpdatePrefixes({});
+	EXPECT_EQ((std::vector<std::pair<uint16_t, rte_flow*>>{{2, handle}, {2, handle}}), destroyed);
+	storage.UpdatePrefixes({});
+	EXPECT_EQ(2u, destroyed.size());
+}
+
+TEST_F(RteFlowTest, PrefixReintroducedAfterFailedDestroyReusesExistingFlow)
+{
+	RteFlowStorage storage;
+	storage.AddPortAndQueue(2, 1);
+	const std::set<common::ip_prefix_t> prefixes{common::ip_prefix_t("192.0.2.0/24")};
+	storage.UpdatePrefixes(prefixes);
+	fail_destroy = true;
+	storage.UpdatePrefixes({});
+	storage.UpdatePrefixes(prefixes);
+	EXPECT_EQ(1u, rules.size());
+	EXPECT_EQ(1u, destroyed.size());
+}
+
+TEST_F(RteFlowTest, UpdateCreatesFlowForPortRegisteredAfterPreviousUpdate)
+{
+	RteFlowStorage storage;
+	const std::set<common::ip_prefix_t> prefixes{common::ip_prefix_t("192.0.2.0/24")};
+	storage.UpdatePrefixes(prefixes);
+	storage.AddPortAndQueue(2, 1);
+	storage.UpdatePrefixes(prefixes);
+	ASSERT_EQ(1u, rules.size());
+	storage.AddPortAndQueue(2, 1);
+	storage.UpdatePrefixes(prefixes);
+	EXPECT_EQ(1u, rules.size());
+}
+
+TEST_F(RteFlowTest, AssignsDistinctIsolatedCoresToPortsOnSameSocket)
+{
+	IsolationDataPlane dataplane;
+	dataplane.config.workers_isolated_cp = {8, 10};
+	const nlohmann::json ports = {
+	        {{"interfaceName", "first"}, {"pci", "first"}, {"coreIds", {4}}, {"rssFlags", {"IPV4"}}},
+	        {{"interfaceName", "second"}, {"pci", "second"}, {"coreIds", {6}}, {"rssFlags", {"IPV4"}}}};
+
+	ASSERT_EQ(dataplane.parseJsonPorts(ports), eResult::success);
+	EXPECT_EQ(dataplane.config.workers.at(8), std::vector<std::string>{"first"});
+	EXPECT_EQ(dataplane.config.workers.at(10), std::vector<std::string>{"second"});
+}
+
+TEST_F(RteFlowTest, UsesActualQueueMapForIsolatedAndOrdinaryTraffic)
+{
+	IsolationDataPlane dataplane;
+	dataplane.config.workers_isolated_cp = {2};
+	const nlohmann::json ports = {
+	        {{"interfaceName", "test"}, {"pci", "test"}, {"coreIds", {4, 6}}, {"rssFlags", {"IPV4"}}}};
+	ASSERT_EQ(dataplane.parseJsonPorts(ports), eResult::success);
+	dataplane.ports[7] = {"test", {{2, 0}, {4, 1}, {6, 2}}, 3, {}, "test", false};
+
+	dataplane.StartIsolatedControlPlane();
+
+	ASSERT_EQ(rules.size(), 5u);
+	for (const auto& rule : rules)
+	{
+		EXPECT_EQ(rule.port, 7);
+		if (rule.action == RTE_FLOW_ACTION_TYPE_QUEUE)
+		{
+			EXPECT_EQ(rule.queue, 0);
+		}
+		else
+		{
+			EXPECT_EQ(rule.action, RTE_FLOW_ACTION_TYPE_RSS);
+			EXPECT_EQ(rule.rss_queues, (std::vector<uint16_t>{1, 2}));
+		}
+	}
 }
